bf813cdaeef6fe6b7a52071893e4e694fa2ff992
aldzikridp <aldzikridp@gmail.com>
2023-12-08 15:58:58 +0700

    use gopass

--- dot_config/sway/executable_pass.sh
+++ dot_config/sway/executable_pass.sh
@@ -1,36 +1,30 @@
 #!/usr/bin/env bash
 
-PASSWORD_STORE_PATH="${PASSWORD_STORE_DIR:-$HOME/.password-store}"
-WHICH_PATH="$(find "$PASSWORD_STORE_PATH" -maxdepth 1 -type d -exec realpath --relative-to "$PASSWORD_STORE_PATH" {} \; | sed '/^\./d' | fzf)"
-
-if [[ -z $WHICH_PATH ]]; then
-  exit 1
-fi
-
 function lists_password(){ 
-  find "${PASSWORD_STORE_PATH}/${WHICH_PATH}" -name '*.gpg' -exec realpath --relative-to "${PASSWORD_STORE_PATH}" {} \; | sed "s/\.gpg$//" | fzf
+  gopass list -f | fzf
 }
 
 function get_username() {
-  pass show "$PASS" | sed -e '2 !d' -e "s/^login:\ //" | wl-copy --trim-newline
+  #gopass show "$PASS" login | wl-copy --trim-newline
+  gopass -c "$PASS" login > /dev/null
 }
 
 function get_password() {
-  pass show "$PASS" | head -1 | wl-copy --trim-newline
+  gopass -c "$PASS" > /dev/null
+}
+
+function get_otp() {
+  gopass otp -c "$PASS" > /dev/null
 }
 
 PASS="$(lists_password)"
-
-IS_OTP="$(pass show "$PASS" | head -1 | grep '^otpauth:\/\/' | wc -l)"
-if [[ $IS_OTP -eq 1 ]]; then
-  pass otp "$PASS" | wl-copy --trim-newline
-else
 
 while [ -n "$PASS" ];
 do
 RESP=$(cat <<EOF | fzf
 username
 password
+otp
 EOF
 );
   
@@ -40,6 +34,9 @@ case "$RESP" in
     ;;
   password)
     get_password
+    ;;
+  otp)
+    get_otp
     break
     ;;
   *)
